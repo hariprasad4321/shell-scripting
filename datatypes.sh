dc6102102e787325673ea0556ef2617f6f2b22bf
hariprasadshetti <shettihari133@gmail.com>
2025-01-01 18:04:34 +0530

    added data types

---
+++ datatypes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NUMBER=$1
+NUMBER=$2
+TIMESTAMP=$(date)
+echo "Script excuted at: $TIMESTAMP"
+SUM=$(($NUMBER1+$NUMBER2))
+echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
--- variablesdeclare.sh
+++ variablesdeclare.sh
@@ -2,6 +2,6 @@
 PERSON1=RAMESH
 PERSON2=SURESH
 echo "$PERSON1: Hi $PERSON2"
-echo "Rahim: Hello $PERSON2"
+echo "$PERSON1: Hello $PERSON2"
 echo "$PERSON1: How are you $PERSON2"
-echo  "Rahim: Iam fine $PERSON1"
+echo  "$PERSON1: Iam fine $PERSON2"
