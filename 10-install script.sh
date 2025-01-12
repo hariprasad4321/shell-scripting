ef20291a78ddd90d8e2cc2017535d1dc5577dd38
hariprasadshetti <shettihari133@gmail.com>
2025-01-12 17:19:36 +0530

    added conditions

--- 10-install script.sh
+++ 10-install script.sh
@@ -4,5 +4,19 @@ then
     echo "ERROR:: throw the error you wont access root permission"
     exit 1
 fi
-dnf install mysql -y   
+dnf install mysql -y 
+if [ $? -ne 0 ]
+exit 1
+then
+echo "mysql installing....failure"
+else
+echo "mysql installing....success"
+fi  
 dnf install git -y 
+if [ $? -ne 0 ]
+exit 1
+then
+echo "git installing....failure"
+else
+echo "git installing....success"
+fi
