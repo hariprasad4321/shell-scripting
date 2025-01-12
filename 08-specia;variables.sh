5e86d122251678c4a67cc404435b9c70c879e32e
hariprasadshetti <shettihari133@gmail.com>
2025-01-12 09:39:14 +0530

    added data

--- 08-specia;variables.sh
+++ 08-specia;variables.sh
@@ -2,6 +2,8 @@ echo "All varaiables passed: $@"
 echo "number of varaiables: $#"
 echo "present script name: $0"
 echo "present working directory: $PWD"
+echo "present home directory: $HOME"
 echo "which user is running script: $USER"
 echo "process id of current script: $$"
+sleep 60 &
 echo "process id of background script: $!"
