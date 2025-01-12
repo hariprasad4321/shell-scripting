f4b7a1210704cc3ec33cffeac4957555d53d7a7e
hariprasadshetti <shettihari133@gmail.com>
2025-01-12 09:17:49 +0530

    added data

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,5 +1,6 @@
-MOVIES=("pushpa","rrr","devara")
+MOVIES=("pushpa" "rrr" "devara")
 #index starts from 0
 echo "first movie: ${MOVIES[0]}"
 echo "second movie: ${MOVIES[1]}"
 echo "third movie: ${MOVIES[2]}"
+echo "all movies are: ${MOVIES[@]}"
