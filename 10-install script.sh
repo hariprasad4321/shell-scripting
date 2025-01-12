1e1ff56db0ce77140bdb0deaecca17e3dea44b8c
hariprasadshetti <shettihari133@gmail.com>
2025-01-12 17:11:26 +0530

    added conditions

--- 10-install script.sh
+++ 10-install script.sh
@@ -1,6 +1,7 @@
 USERID=$(id -u)
-if [ $USERID -ne o]
+if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: throw the error you wont access root permission"
 fi
-dnf install mysql -y    
+dnf install mysql -y   
+dnf install git -y 
