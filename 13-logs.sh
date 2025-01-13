5972f49b9bf2bb8da1b84d911361ff978635646f
hariprasadshetti <shettihari133@gmail.com>
2025-01-13 15:27:26 +0530

    added the script

---
+++ 13-logs.sh
@@ -0,0 +1,40 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="var/log/shell-script.logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+   echo -e "$2....$R failure"
+   exit 1
+else
+    echo -e "$2....$G success"
+fi
+}
+echo "script started and excuted at:: $TIMESTAMP" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: throw the error you wont access root permission"
+    exit 1
+fi
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing Mysql"
+else
+    echo -e "print mysql is already.... $Y installed"
+fi
+dnf list installed git &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+dnf install git -y
+VALIDATE $? "Installing Git" &>>$LOG_FILE
+else
+    echo -e "print git alraedy.... $Y installed"
+fi
