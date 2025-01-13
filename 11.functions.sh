2b651a0b3b4fd6a4e880489bfd56e97fd0d629ed
hariprasadshetti <shettihari133@gmail.com>
2025-01-13 09:35:22 +0530

    added function

---
+++ 11.functions.sh
@@ -0,0 +1,30 @@
+USERID=$(id -u)
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+   echo "$2....failure"
+   exit 1
+else
+    echo "$2....success"
+}
+fi
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: throw the error you wont access root permission"
+    exit 1
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+dnf install mysql -y 
+VALIDATE $? "Installing Mysql"
+else
+    echo "print mysql is already installed"
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+dnf install git -y
+VALIDATE $? "Installing Git"
+else
+    echo "print git alraedy installed"
+fi
