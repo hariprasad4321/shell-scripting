3b1dd0201b888b7a232973f86168428689be5fbd
hariprasadshetti <shettihari133@gmail.com>
2025-01-13 10:44:57 +0530

    added function

---
+++ 12-colours.sh
@@ -0,0 +1,35 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+   echo "$2....$R failure"
+   exit 1
+else
+    echo "$2....$G success"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: throw the error you wont access root permission"
+    exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+dnf install mysql -y 
+VALIDATE $? "Installing Mysql"
+else
+    echo -e "print mysql is already.... $Y installed"
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+dnf install git -y
+VALIDATE $? "Installing Git"
+else
+    echo -e "print git alraedy.... $Y installed"
+fi
