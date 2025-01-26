9d112fdb1c21bfa2d6ef5b64d92dc2be24136f8a
hariprasadshetti <shettihari133@gmail.com>
2025-01-26 14:00:53 +0530

    added logs folders

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/-$TIMESTAMP.log"
@@ -30,6 +30,7 @@ if [ $# -lt 2 ]
 then
     USAGE   
 fi
+
 if [ !-d $SOURCE_DIR ]
 then
     echo -e "$SOURCE_DIR does not exist ..... please check"
@@ -41,5 +42,5 @@ if [ ! -d $DEST_DIR ]
 then 
     echo -e "$DEST_DIR does not exist ......please check"
     exit 1
-if
+fi
 echo "script started at and excuted at: $TIMESTAMP"
